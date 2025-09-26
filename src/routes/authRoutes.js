fa4ae0e2117cdecaa82c62acc71b11656e0aae0d
Reegan <reegank20@gmail.com>
2025-09-26 23:59:44 +0530

    Expose forgotPassword API endpoint

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const { signup, verifySignup, login } = require("../controllers/authController");
+const { signup, verifySignup, login, forgotPassword } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signup/verify", verifySignup);
 router.post("/login", login);
-// router.post("/forgot-password", forgotPassword)
+router.post("/forgot-password", forgotPassword)
 
 module.exports = router;
 
