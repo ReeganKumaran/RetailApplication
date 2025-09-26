8fbc0df3f33c6848a9e99417d99764eaafca0b22
Reegan <reegank20@gmail.com>
2025-09-26 23:33:22 +0530

    Prepare route for forgotPassword endpoint

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post("/signup", signup);
 router.post("/signup/verify", verifySignup);
 router.post("/login", login);
+// router.post("/forgot-password", forgotPassword)
 
 module.exports = router;
 
