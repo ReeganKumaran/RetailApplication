6b133e7c763832ad9f3d3d0c62795cda9cad9107
Reegan <reegank20@gmail.com>
2025-10-31 02:30:44 +0530

    refactor: update MongoDB connection logic to use a variable for URI

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,15 @@
 const mongoose = require("mongoose");
+let MONGODB_URI = process.env.MONGODB_LOCAL_URI;
+if (process.env.NODE_ENV === "production") {
+  MONGODB_URI = process.env.MONGODB_URI;
+}
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI);
-        console.log("MongoDB connected successfully");
-    } catch (error) {
-        console.error("MongoDB connection failed:", error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
 };
 module.exports = connectDB;
