d223db3117562e5e2229bb8822f31109547a3929
Reegan <reegank20@gmail.com>
2025-10-30 23:27:20 +0530

    refactor: simplify server startup by removing HOST variable and updating listen method

--- src/server.js
+++ src/server.js
@@ -1,16 +1,12 @@
 require("dotenv").config();
 const app = require("./app");
 const connectDB = require("./config/db");
-const PORT = process.env.PORT || "5000";
-const HOST = "0.0.0.0";
+
+const PORT = process.env.PORT || 5000;
 
 (async () => {
   await connectDB();
-  app.listen(PORT, HOST, () => {
-    console.log(
-      `Server is running at http://${
-        HOST === "0.0.0.0" ? "your-lan-ip" : HOST
-      }:${PORT}`
-    );
+  app.listen(PORT, () => {
+    console.log(`✅ Server running on port ${PORT}`);
   });
 })();
